3db29824d31bab1a290ca89c5da8ae87c623d3d4
Lukasz Siwek <xofczarx@gmail.com>
2017-05-24 15:39:33 +0200

    Implement repository pattern.

---
+++ CityInfo/src/CityInfo.API/Services/ICityInfoRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using CityInfo.API.Entities;
+
+namespace CityInfo.API.Services
+{
+    interface ICityInfoRepository
+    {
+        IEnumerable<City> GetCities();
+        City GetCity(int cityId, bool includePointOfInterest);
+        IEnumerable<PointOfInterest> GetPointOfInterestsForCity(int cityId);
+        PointOfInterest GetPointOfInterestForCity(int cityId, int pointOfInterestId);
+    }
+}
